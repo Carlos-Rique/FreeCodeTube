4e0ebed62d3a7b4c5030b68fcc920c2ea6ae8c8c
Carlos Silva <carlosrique13@hotmail.com>
2020-10-25 21:59:58 -0400

    Model video created

---
+++ backend/views/video/create.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Video */
+
+$this->title = 'Create Video';
+$this->params['breadcrumbs'][] = ['label' => 'Videos', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="video-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+  <div class="d-flex flex-column justify-content-center align-items-center">
+      <div class="upload-icon">
+        <i class="fas fa-upload"></i>
+      </div>
+
+      <br>
+
+      <p class="m-2">Drag and drop a file you want to be upload</p>
+      <p class="text-muted">Your video will be private until you publish it</p>
+
+      <button class="btn btn-primary btn-file">
+            Select file
+            <input type="file" name="video" id="videoFile">
+      </button>
+  </div>
+
+</div>
